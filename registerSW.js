f48fa484cbd14733ad948b1210fc81286ee06b68
Gowwre <56794916+Gowwre@users.noreply.github.com>
2026-02-16 15:26:50 +0000

    Deploying to public from @ Gowwre/company-sim@d0a46fb448860428db1bf9056966406c2a817b49 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/company-sim/sw.js', { scope: '/company-sim/' })})}
